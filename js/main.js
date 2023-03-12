7e7906074e38033a02c5afc4e02d7002a39a026d
Gerald Heston <gerald@hestongis.com>
2023-03-12 14:08:47 -0700

    finished lesson 2-3
    created a bubble chart with lables

--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@
 var w = 900, h = 500;
 
 
+
 //execute script when window is loaded
-window.onload = function() {
+window.onload = function () {
     var container = d3.select("body") //get the <body> element from the DOM
         .append("svg") // put a new svg in the body
         .attr("width", w) // assign the width
@@ -12,24 +13,161 @@ window.onload = function() {
         .attr("class", "container") // always assign a class (as the block name) for styling and future selection
         .style("background-color", "rgba(0,0,0,0.2)");
 
-// innerRect block
-var innerRect = container.append("rect") // add a <rect> element
-    .datum(400)    
-    .attr("width", function(d){ // rectangle width
-        return d * 2; //400 * 2 = 800
-    })
-     
-    .attr("height", function(d){
-        return d; //rectangle height
-    })
-    .attr("class", "innerRect") // class name
-    .attr("x", 50) // position from left on the x (horizontal) axis
-    .attr("y", 50) // positio from left on the y (vertical) axis
-    .style("fill", "#FFFFFF"); // fill color
-    
+    // innerRect block
+    var innerRect = container.append("rect") // add a <rect> element
+        .datum(400)
+        .attr("width", function (d) { // rectangle width
+            return d * 2; //400 * 2 = 800
+        })
+
+        .attr("height", function (d) {
+            return d; //rectangle height
+        })
+        .attr("class", "innerRect") // class name
+        .attr("x", 50) // position from left on the x (horizontal) axis
+        .attr("y", 50) // positio from left on the y (vertical) axis
+        .style("fill", "#FFFFFF"); // fill color
+
 
     //console.log(innerRect);
 
-};
+
+    var dataArray = [10, 20, 30, 40, 50];
+
+    var cityPop = [
+        {
+            city: 'Madison',
+            population: 233209
+        },
+        {
+            city: 'Milwaukee',
+            population: 594833
+        },
+        {
+            city: 'Green Bay',
+            population: 104057
+        },
+        {
+            city: 'Superior',
+            population: 27244
+        }
+    ];
+
+    // find the minimum value of the citiPop array
+    var minPop = d3.min(cityPop, function (d) {
+        return d.population;
+    });
+
+    //find the maximum value of the citiPop array
+    var maxPop = d3.max(cityPop, function (d) {
+        return d.population;
+    });
+
+
+    var x = d3.scaleLinear() // create the scale
+        .range([90, 755]) // output min and max
+        .domain([0, 3]); // input min and max
+
+    var y = d3.scaleLinear()
+        .range([450, 50])
+        .domain([
+            0, // was minPop
+            700000 // was maxPop - made them go a bit beyond to make the axis look better
+        ]);
+
+    // color scale generator
+    var color = d3.scaleLinear()
+        .range([
+            "#FDBE85",
+            "#D94701"
+        ])
+        .domain([
+            minPop,
+            maxPop
+        ]);
+
+    var circles = container.selectAll(".circles")
+        .data(cityPop) // here we feed an array
+        .enter() //one of the great mysteries of the universe
+        .append("circle") // add a circle for each datum
+        .attr("class", "circles") // apply a class name to all circles
+        .attr("id", function (d) {
+            return d.city;
+        })
+        .attr("r", function (d, i) {
+            // calculate the radius based on population value as circle area
+            var area = d.population * 0.01;
+            return Math.sqrt(area / Math.PI);
+        })
+        .attr("cx", function (d, i) {
+            //use the scale generator with the index to place each circle horizontally
+            return x(i);
+        })
+        .attr("cy", function (d) {
+            //subtract value from 450 to "grow" circles up from bottom instead of down from the top of the SVG
+            return y(d.population);
+        })
+        .style("fill", function (d, i) { // add a fill based on the color scale generator
+            return color(d.population);
+        })
+        .style("stroke", "#000"); // black circle stroke
+
+
+    // y axis generator
+    var yAxis = d3.axisLeft(y);
+
+    // create axis <g> element and add axis
+    var axis = container.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(50,0)")
+        .call(yAxis);
+
+    var title = container.append("text")
+        .attr("class", "title")
+        .attr("text-anchor", "middle")
+        .attr("x", 450)
+        .attr("y", 30)
+        .text("City Populations");
+
+    var labels = container.selectAll(".labels")
+        .data(cityPop)
+        .enter()
+        .append("text")
+        .attr("class", "labels")
+        .attr("text-anchor", "left")
+        .attr("y", function (d) {
+            //vertical position centered on each circle
+            return y(d.population) - 3;
+        });
+
+    // first line of label
+    var nameLine = labels.append("tspan")
+        .attr("class", "nameLine")
+        .attr("x", function (d, i) {
+            // horizontal position to the right of each circle
+            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+        })
+        .text(function (d) {
+            return d.city;
+        });
+
+var format = d3.format(",");
+
+
+    // second line of label
+    var popLine = labels.append("tspan")
+        .attr("class", "popLine")
+        .attr("x", function (d, i) {
+            // horizontal position to the right of each circle
+            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+        })
+        .attr("dy", "15") // vertical offset
+        .text(function (d) {
+            return "Pop. " + format(d.population);
+        });
+
+
+
+    };
 
 
